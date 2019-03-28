091eac2582979600016be2d7db6f4f85f4617a59
Chris Chamberlain <cj.chamberlain1385@gmail.com>
2019-03-28 07:59:20 -0400

    commented out the mongo insert fucntion in main.js

--- server/main.js
+++ server/main.js
@@ -2,15 +2,15 @@ import { Meteor } from "meteor/meteor";
 import { GrowData } from "../imports/api/growdata.js";
 
 Meteor.startup(function() {
-  //server side stuff
-
+  //server side stuff for db insertion
+  /*
   GrowData.insert({
     sensorName: "Temperature",
     sensorValue: "28",
     createdAt: Date().toString()
   });
   console.log(GrowData.find().fetch());
-
+*/
 
   if(Meteor.isServer) {
     Meteor.publish('GrowData', function() {
